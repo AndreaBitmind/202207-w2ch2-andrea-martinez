247440164950cbb026460bcc8b0734bbfeabd8e2
Andrea Martinez <andrea.maort@gmail.com>
2022-07-12 20:03:19 +0200

    comparison 2 done

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const a = 1;
-const b = 1;
-let result;
-
 const strictEquals = function strictEquals(a, b) {
+  if (Number.isNaN(a) && Number.isNaN(b)) {
+    return false;
+  }
+
   if (Object.is(a, b)) {
     return true;
   }
 };
 
 strictEquals();
+
+console.log(strictEquals(1, 1));
+console.log(strictEquals(NaN, NaN));
+console.log(strictEquals(0, -0));
+console.log(strictEquals(-0, 0));
+console.log(strictEquals(1, "1"));
+console.log(strictEquals(true, false));
+console.log(strictEquals(false, true));
+console.log(strictEquals("Water", "oil"));
